219d86f13870d9348e02b67281ac1ea940d5765d
Kainoa Kanter <kainoa@t1c.dev>
2022-03-04 15:54:29 -0800

    test thingys

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,138 +1,147 @@
-let lineNumbers = $(".line-numbers");
-let editor = $("#text-area");
-let codeViewPre = $("#code-view-pre");
-let codeView = $("#code-view");
-let messages = $("#messages");
+const jquery = require("jquery");
+const hljs = require("highlight.js");
 
-let saveButton = $("#save-button");
-let newButton = $("#new-button");
+window.$ = window.jQuery = jquery;
 
-let apiUrl = "http://localhost:8000";
+const lineNumbers = $(".line-numbers");
+const editor = $("#text-area");
+const codeViewPre = $("#code-view-pre");
+const codeView = $("#code-view");
+const messages = $("#messages");
+
+const saveButton = $("#save-button");
+const newButton = $("#new-button");
+
+const apiUrl = "http://localhost:8000";
+
+hljs.highlightAll();
 
 function postPaste(content, callback) {
-	var data = {
-		content,
-	};
+    const data = {
+        content,
+    };
 
-	$.ajax({
-		type: "POST",
-		url: `${apiUrl}/p/n`,
-		data: JSON.stringify(data),
-		dataType: "json",
-		contentType: "application/json",
-		success: function (res) {
-			callback(null, res);
-		},
-		error: function (xhr) {
-			callback(
-				JSON.parse(
-					xhr.responseText ||
-						`{"data": { "message": "An unkown error occured!" } }`
-				)
-			);
-		},
-	});
+    $.ajax({
+        type: "POST",
+        url: `${apiUrl}/p/n`,
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        crossDomain: true,
+        success: function(res) {
+            callback(null, res);
+        },
+        error: function(xhr) {
+            callback(
+                JSON.parse(
+                    xhr.responseText ||
+                    `{"data": { "message": "An unkown error occured!" } }`
+                )
+            );
+        },
+    });
 }
 
 function getPaste(id, callback) {
-	$.ajax({
-		type: "GET",
-		url: `${apiUrl}/p/${id}`,
-		contentType: "application/json",
-		success: function (res) {
-			callback(null, res);
-		},
-		error: function (xhr) {
-			callback(
-				JSON.parse(
-					xhr.responseText ||
-						`{"data": { "message": "Unknown error occurred.." } }`
-				)
-			);
-		},
-	});
+    $.ajax({
+        type: "GET",
+        url: `${apiUrl}/p/${id}`,
+        contentType: "application/json",
+        crossDomain: true,
+        success: function(res) {
+            callback(null, res);
+        },
+        error: function(xhr) {
+            callback(
+                JSON.parse(
+                    xhr.responseText ||
+                    `{"data": { "message": "Unknown error occurred.." } }`
+                )
+            );
+        },
+    });
 }
 
 function newPaste() {
-	lineNumbers.html("<div>&gt;</div>");
+    lineNumbers.html("<div>&gt;</div>");
 
-	saveButton.prop("disabled", false);
-	newButton.prop("disabled", true);
+    saveButton.prop("disabled", false);
+    newButton.prop("disabled", true);
 
-	editor.val("");
+    editor.val("");
 
-	editor.show();
-	codeViewPre.hide();
+    editor.show();
+    codeViewPre.hide();
 }
 
 function addMessage(message) {
-	let msg = $(`<li>${message}</li>`);
-	messages.prepend(msg);
+    let msg = $(`<li>${message}</li>`);
+    messages.prepend(msg);
 
-	setTimeout(function () {
-		msg.slideUp("fast", function () {
-			$(this).remove();
-		});
-	}, 3000);
+    setTimeout(function() {
+        msg.slideUp("fast", function() {
+            $(this).remove();
+        });
+    }, 3000);
 }
 
 function createTextLinks(text) {
-	return (text || "").replace(
-		/([^\S]|^)(((https?\:\/\/)|(www\.))(\S+))/gi,
-		function (match, space, url) {
-			let hyperlink = url;
-			if (!hyperlink.match("^https?://")) {
-				hyperlink = "http://" + hyperlink;
-			}
-			return space + '<a href="' + hyperlink + '">' + url + "</a>";
-		}
-	);
+    return (text || "").replace(
+        /([^\S]|^)(((https?\:\/\/)|(www\.))(\S+))/gi,
+        function(match, space, url) {
+            let hyperlink = url;
+            if (!hyperlink.match("^https?://")) {
+                hyperlink = "http://" + hyperlink;
+            }
+            return space + '<a href="' + hyperlink + '">' + url + "</a>";
+        }
+    );
 }
 
 function viewPaste(content) {
-	lineNumbers.html("");
-	for (let i = 1; i <= content.split("\n").length; i++) {
-		lineNumbers.append(`<div>${i}</div>`);
-	}
-	codeView.html(createTextLinks(content));
-	editor.hide();
-	codeViewPre.show();
+    lineNumbers.html("");
+    for (let i = 1; i <= content.split("\n").length; i++) {
+        lineNumbers.append(`<div>${i}</div>`);
+    }
+    codeView.html(createTextLinks(content));
+    editor.hide();
+    codeViewPre.show();
 }
 
-saveButton.click(function () {
-	if (editor.val() === "") {
-		return;
-	}
+saveButton.click(function() {
+    if (editor.val() === "") {
+        return;
+    }
 
-	postPaste(editor.val(), function (err, res) {
-		if (err) {
-			addMessage(err["data"]["message"]);
-		} else {
-			window.location.href = `/${res["data"]["id"]}`;
-		}
-	});
+    postPaste(editor.val(), function(err, res) {
+        if (err) {
+            addMessage(err["data"]["message"]);
+        } else {
+            window.location.href = `/${res["data"]["id"]}`;
+        }
+    });
 });
 
-newButton.click(function () {
-	window.location.href = "/";
+newButton.click(function() {
+    window.location.href = "/";
 });
 
-$(document).ready(function () {
-	let path = window.location.pathname;
+$(document).ready(function() {
+    let path = window.location.pathname;
 
-	if (path == "/") {
-		newPaste();
-	} else {
-		let id = path.substring(1, path.length);
+    if (path == "/") {
+        newPaste();
+    } else {
+        let id = path.substring(1, path.length);
 
-		getPaste(id, function (err, res) {
-			if (err) {
-				newPaste();
-			} else {
-				let content = res["data"]["content"];
-				viewPaste(hljs.highlightAuto(content).value);
-				saveButton.prop("disabled", true);
-			}
-		});
-	}
+        getPaste(id, function(err, res) {
+            if (err) {
+                newPaste();
+            } else {
+                let content = res["data"]["content"];
+                viewPaste(hljs.highlightAuto(content).value);
+                saveButton.prop("disabled", true);
+            }
+        });
+    }
 });
