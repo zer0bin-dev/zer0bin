783dabbe9f763056f75b3d95dbad8951c3778edc
ThatOneCalculator <kainoa@t1c.dev>
2022-03-19 10:41:48 -0700

    Close 🐢 Split TS files #21

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,15 +1,8 @@
-import {
-	SaveOutlined,
-	FileAddOutlined,
-	GithubOutlined,
-	CopyOutlined,
-} from "@ant-design/icons-svg"
-import { renderIconDefinitionToSVGElement } from "@ant-design/icons-svg/es/helpers"
 import hljs from "highlight.js"
 import JSConfetti from "js-confetti"
 import Scrollbar from "smooth-scrollbar"
 
-const config = require("../config.json")
+import config from "../config.json"
 const apiUrl = config.api_url
 const jsConfetti = new JSConfetti()
 
@@ -30,26 +23,6 @@ const viewCounter = <HTMLSpanElement>(
 const saveButton = <HTMLButtonElement>document.getElementById("save-button")
 const newButton = <HTMLButtonElement>document.getElementById("new-button")
 const copyButton = <HTMLButtonElement>document.getElementById("copy-button")
-const githubButton = <HTMLButtonElement>document.getElementById("github-button")
-
-const extraSVGAttrs = {
-	width: "1em",
-	height: "1em",
-	fill: "currentColor",
-}
-
-saveButton.innerHTML += renderIconDefinitionToSVGElement(SaveOutlined, {
-	extraSVGAttrs: extraSVGAttrs,
-})
-newButton.innerHTML += renderIconDefinitionToSVGElement(FileAddOutlined, {
-	extraSVGAttrs: extraSVGAttrs,
-})
-copyButton.innerHTML += renderIconDefinitionToSVGElement(CopyOutlined, {
-	extraSVGAttrs: extraSVGAttrs,
-})
-githubButton.innerHTML += renderIconDefinitionToSVGElement(GithubOutlined, {
-	extraSVGAttrs: extraSVGAttrs,
-})
 
 function hide(element: HTMLElement) {
 	element.style.display = "none"
