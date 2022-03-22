a66540ca5ad1cd154402175e1102eb86c733a9e7
ThatOneCalculator <kainoa@t1c.dev>
2022-03-21 17:45:44 -0700

    refactor: :lipstick: CSS -> SCSS

--- frontend/src/icons.ts
+++ frontend/src/icons.ts
@@ -6,7 +6,7 @@ import {
 } from "@ant-design/icons-svg"
 import { renderIconDefinitionToSVGElement } from "@ant-design/icons-svg/es/helpers"
 import tippy from "tippy.js"
-import "../style/tooltip.css"
+import "../style/tooltip.scss"
 import "tippy.js/animations/scale.css"
 
 const saveButton = <HTMLButtonElement>document.getElementById("save-button")
