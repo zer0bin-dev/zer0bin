21ec4c1ef06b470eab278475dfd0b1601268b947
ThatOneCalculator <kainoa@t1c.dev>
2022-04-07 19:35:47 -0700

    perf: :zap: Add vite-plugin-compression for gzip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import pugPlugin from "vite-plugin-pug"
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-    plugins: [pugPlugin()],
+    plugins: [pugPlugin(), viteCompression()],
 })
